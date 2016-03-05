0e6c4135539d9387af7ac3f8829d664a28bb3ecb
Eric Geniesse <egeniesse@gmail.com>
2016-03-04 16:09:04 -0800

    first commit finishing unique paths algorithm

---
+++ src/unique-paths.js
@@ -0,0 +1,35 @@
+// This algorithm is to determine all of the paths a robot might take to get from the top right corner
+// of an NxM matrix to the bottom left corner of that matrix. The robot can only move down and right. 
+// 
+// ex. uniquePaths(1,3)   // 1
+//     uniquePaths(2,2)   // 2
+ 
+var uniquePaths = function (n, m){
+  if(n < 0 || m < 0){
+    return -1;
+  }
+  // make a results variable that tracks every unique path
+  var results = 0;
+
+  // make a subroutine that traverses the matrix using only unused paths
+  var traverseMatrix = function (x, y) {
+
+    if(x === n-1 && y === m-1){
+      results++; // once the base case is reached, increment the counter.
+
+    } else {
+      if (x < n-1) {
+        traverseMatrix(x+1, y);
+      }
+      if (y < m-1) {
+        traverseMatrix(x, y+1);
+      }
+    }
+  };
+
+  traverseMatrix(0,0);
+
+  return results;
+};
+
+module.exports = uniquePaths;
---
+++ test/minitest_globs/unique-paths-spec.js
@@ -0,0 +1,25 @@
+var mocha = require('mocha');
+var chai = require('chai');
+var expect = chai.expect;
+var uniquePaths = require('../src/unique-paths.js');
+
+describe('The unique paths solution', function(){
+	
+	it('Should return 0 when given a 0x0 matrix', function(done){
+		expect(uniquePaths(0,0)).to.equal(0);
+		done();
+	});
+
+	it('Should return -1 if m or n is less than 0', function(done){
+		expect(uniquePaths(-1,5)).to.equal(-1);
+		expect(uniquePaths(1,-5)).to.equal(-1);
+		done();
+	});
+
+	it('Should take any coordinates and return the correct number of paths', function(done){
+		expect(uniquePaths(10,4)).to.equal(220);
+		expect(uniquePaths(3,5)).to.equal(15);
+		expect(uniquePaths(8,23)).to.equal(1560780);
+		done();
+	});
+});
